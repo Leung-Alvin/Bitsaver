6a9d0ae0ac3ee735ce5cb539732b9ae216f677a0
Leung-Alvin <89597642+Leung-Alvin@users.noreply.github.com>
2025-12-01 20:41:55 -0800

    new work

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import shutil
 import subprocess
 from dataclasses import dataclass
 from typing import List, Optional, Dict, Any
+from os import system
 
 from PySide6.QtCore import Qt, QThread, Signal
 from PySide6.QtWidgets import (
@@ -19,7 +20,8 @@ from PySide6.QtWidgets import (
 )
 
 import pyuac
-
+RECOVERY_DIR = "./recovery_keys"
+SECURE_STRING = "1234"
 def run_powershell(ps_command: str) -> subprocess.CompletedProcess:
     exe = shutil.which("powershell") or shutil.which("powershell.exe")
     if not exe:
@@ -139,8 +141,31 @@ def get_bitlocker_status():
     else:
         return ("Error:", results.stderr)
 
+def select_disk(disk_number, disks):
+    if disks[disk_number].is_boot == True:
+        return ("Error: Cannot select Boot disk.")
+    else:
+        return disks[disk_number]
 
-def main():
+def enable_bitlocker(mount_point, SECURE_STRING):
+    formatted_mount_point = mount_point.replace("[","").replace(']',"")
+    print(formatted_mount_point)
+#    ps = "Enable-BitLocker -MountPoint " + "\""
+#    results = subprocess.run(["powershell", "-Command", ps ], capture_output=True, text=True)
+#    if results.returncode == 0:
+#        return (results.stdout)
+#    else:
+#        return ("Error:", results.stderr)
+
+def get_bitlocker_status():
+    ps = "Get-BitLockerVolume | Select-Object -Property VolumeType, MountPoint, CapacityGB, VolumeStatus | ConvertTo-Json"
+    results = subprocess.run(["powershell", "-Command", ps ], capture_output=True, text=True)
+    if results.returncode == 0:
+        return (results.stdout)
+    else:
+        return ("Error:", results.stderr)
+def print_disks():
+    #system('cls')
     print("Computer Name:", get_hostname())
     val = get_bitlocker_status()
     val_json = json.loads(val)
@@ -148,7 +173,6 @@ def main():
         val_json = list() 
         print(json.loads(val))
         val_json = val_json.append(json.loads(val))
-    print("after",val_json)
     for jso in val_json:
        jso['MountPoint'] = jso['MountPoint'].replace(':','')
        if jso['VolumeType'] == 0:
@@ -173,10 +197,132 @@ def main():
         attrs['letters'] = ''.join(attrs['letters'])
         print('\n'.join("%s: %s" % item for item in attrs.items()))
         print('\n')
-    input("Press enter to close the window.")
 
-if __name__ == '__main__' :
+def short_print_disks():
+    #system('cls')
+    print('\n')
+    val = get_bitlocker_status()
+    val_json = json.loads(val)
+    if not isinstance(val_json,list):
+        val_json = list() 
+        print(json.loads(val))
+        val_json = val_json.append(json.loads(val))
+    for jso in val_json:
+       jso['MountPoint'] = jso['MountPoint'].replace(':','')
+       if jso['VolumeType'] == 0:
+           jso['VolumeType'] = 'Operating System'
+       elif jso['VolumeType'] == 1:
+           jso['VolumeType'] = 'Data'
+       else:
+           raise ValueError('A new type of Volume has been found:',jso['VolumeType'])
+
+       if jso['VolumeStatus'] == 0:
+           jso['VolumeStatus'] = 'Fully Decrypted'
+       elif jso['VolumeStatus'] == 1:
+           jso['VolumeStatus'] = 'Fully Encrypted'
+       else:
+           raise ValueError('A new type of VolumeStatus has been found:', jso['VolumeStatus'])
+    disks = list_disks()
+    for i in range(len(disks)):
+        disks[i].volume_status= val_json[i]['VolumeStatus']
+        attrs = vars(disks[i])
+        print("Disk Number:",attrs['disk_number'])
+        print("Model Name:",attrs['model_name'])
+        print("Boot Drive:",attrs['is_boot'])
+        print("Drive Letter:",attrs['letters'])
+        print("Bitlocker Status:",attrs['volume_status'])
+        print('\n')
+
+    choice = input("Enter Disk Number: ")
+    print('\n')
+    disk = select_disk(int(choice),disks)
+    if isinstance(disk, DiskInfo):
+        disk_menu(disk)
+    else:
+        print(disk)
+def main_menu():
+    while True:
+        print("\n===Main Menu===")
+        print("1. List Disks")
+        print("2. Select Disk")
+        print("3. Exit")
+        choice = input("Enter choice: ")
+        print('\n')
+
+        if choice == "1":
+            print_disks()
+
+        elif choice == "2":
+            short_print_disks()
+        elif choice == "3":
+            print("Ending")
+            break
+        else:
+            print("Invalid Choice")
+
+def disk_menu(disk):
+    attrs = vars(disk) 
+    print(attrs)
+    while True:
+        print("\n===Disk Menu===")
+        print("1. Encrypt with Bitlocker")
+        print("2. Decrypt with Bitlocker")
+        print("3. Exit")
+        choice = input("Enter choice: ")
+        print('\n')
+
+        if choice == "1":
+            print("testing")
+           # enable_bitlocker(disk.
+
+        elif choice == "2":
+            short_print_disks()
+        elif choice == "3":
+            print("Ending")
+            break
+        else:
+            print("Invalid Choice")
+def main():
+    print("Computer Name:", get_hostname())
+    val = get_bitlocker_status()
+    val_json = json.loads(val)
+    if not isinstance(val_json,list):
+        val_json = list() 
+        print(json.loads(val))
+        val_json = val_json.append(json.loads(val))
+    for jso in val_json:
+       jso['MountPoint'] = jso['MountPoint'].replace(':','')
+       if jso['VolumeType'] == 0:
+           jso['VolumeType'] = 'Operating System'
+       elif jso['VolumeType'] == 1:
+           jso['VolumeType'] = 'Data'
+       else:
+           raise ValueError('A new type of Volume has been found:',jso['VolumeType'])
+
+       if jso['VolumeStatus'] == 0:
+           jso['VolumeStatus'] = 'Fully Decrypted'
+       elif jso['VolumeStatus'] == 1:
+           jso['VolumeStatus'] = 'Fully Encrypted'
+       else:
+           raise ValueError('A new type of VolumeStatus has been found:', jso['VolumeStatus'])
+    disks = list_disks()
+    for i in range(len(disks)):
+        disks[i].volume_type = val_json[i]['VolumeType']
+        disks[i].volume_status= val_json[i]['VolumeStatus']
+        disks[i].capacity_GB = val_json[i]['CapacityGB']
+        attrs = vars(disks[i])
+        attrs['letters'] = ''.join(attrs['letters'])
+        print('\n'.join("%s: %s" % item for item in attrs.items()))
+        print('\n')
+    print(select_disk(1,disks))
+
+def test():
+    print_disks()
+    main_menu()
+
+if __name__ == '__main__':
     if not pyuac.isUserAdmin():
         pyuac.runAsAdmin()
     else:
-        main()
+        #main()
+        test()
